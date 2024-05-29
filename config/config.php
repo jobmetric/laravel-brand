1310ee049dd2b5e6b7d2b679f99f939ffdf40152
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-29 23:49:36 +0330

    add config file

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data brand
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("BRAND_CACHE_TIME", 0),
+
+];
