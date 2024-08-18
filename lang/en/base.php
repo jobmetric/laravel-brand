42fd4091e305632920290d3433c695bbda9f5b4f
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-18 20:47:58 +0330

    add base langauge

---
+++ lang/en/base.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Brand Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Brand for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+        "errors" => "Validation errors occurred."
+    ],
+
+    "messages" => [
+        "found" => "Brand found.",
+        "created" => "Brand created successfully.",
+        "updated" => "Brand updated successfully.",
+        "deleted" => "Brand deleted successfully.",
+        "used_in" => "Brand used in ':count' places.",
+        "attached" => "Brand attached successfully.",
+        "detached" => "Brand detached successfully.",
+    ],
+
+    "exceptions" => [
+        "brand_not_found" => "Brand ':number' not found!",
+        "brand_used_in" => "Brand number ':brand_id' used in ':number' places!",
+    ],
+];
